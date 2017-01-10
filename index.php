28730ed9acf4806c5bf7156773d6a8d4c5c067e3
Fabio Amorim <batista.amorim@hotmail.com>
2017-01-10 20:37:01 -0200

    Inicio de projeto

---
+++ index.php
@@ -0,0 +1,75 @@
+    <?php
+    /*
+     *System: Gerador e validador de CPF v. 1.0
+     *Data:20/08/2016
+     *Autor: Fábio Batista do Amorim
+     */
+      
+      include_once ('_class/calculo.class.php');
+      
+      // verifica se o campo cpf foi preenchido e enviado
+      if(isset($_REQUEST['nCpf'])){
+    
+        $novoCalculo = new calculo();
+    
+        //  verifica se o botao foi precionado
+        if(isset($_POST['verificar'])){       
+           if($novoCalculo->cpf_invalido($_REQUEST['nCpf'])){
+                   if($novoCalculo->calculo_cpf($_REQUEST['nCpf'])){
+                         echo "<script>
+                                  alert('CPF valido!');
+                              </script>";
+                   }else{
+                         echo "<script>
+                                  alert('CPF invalido!');
+                              </script>";
+                   }
+            }else{
+                         echo "<script>
+                                  alert('CPF invalido!');
+                              </script>";
+            }
+        }
+        // Se botão gerar foi precionado
+        if(isset($_POST['gerar'])){
+          $novoCalculo->gerar();
+        }        
+        // Se botão limpar foi precionado
+        if(isset($_POST['limpar'])){
+          header('location: index.php');
+        }
+       
+     }     
+    
+    ?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Validação de CPF</title>
+            <link rel="stylesheet" type="text/css" href="_css/style.css"/>
+            <script type="text/javascript" src="_jquery/jquery-1.2.6.pack.js"></script>
+            <script type="text/javascript" src="_jquery/jquery.maskedinput-1.1.4.pack.js"></script>
+            <script type="text/javascript">$(document).ready(function(){	$("#vCpf").mask("999.999.999-99");});</script>            
+        </head>
+        <body>
+          <div>
+              <form id="form_cpf" method=POST action="?check=true">
+                <fieldset class="field_cpf">
+                   CPF:<br><input type=text name="nCpf" id="vCpf" maxlength=14
+                            <?php                            
+                                 //Este if verifica se check é verdadeiro e se o botão gerar foi precionado e gera um CPF válido aleatório para o text
+                                 if(isset($_REQUEST["check"]) && isset($_POST['gerar'])){echo "value='".$novoCalculo->gerar()."'";}
+                                 if(isset($_REQUEST["check"]) && isset($_POST['verificar'])){echo "value='".$_POST['nCpf']."'";}
+                            ?>
+                           >
+
+                        <br><br>
+                  <input type=submit name = "verificar" value=VERIFICAR >
+                  <input type=submit name = "gerar" value=GERAR>
+                  <input type=submit name = "limpar" value=LIMPAR >
+                </fieldset>                  
+              </form>
+          </div>    
+        </body>
+    </html>
